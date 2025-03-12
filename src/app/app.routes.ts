fcfdc204b26e94f2a177f2cb82478f0880a68f41
Jakub Bryska <troppout@gmail.com>
2025-03-12 09:33:45 +0100

    feat: add starting-page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: StartPageComponent,
+    title: 'ITHelper',
+  },
+];
---
+++ src/app/pages/start-page/start-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './start-page.component.html',
+  styleUrl: './start-page.component.css'
+})
+export class StartPageComponent {
+
+}
