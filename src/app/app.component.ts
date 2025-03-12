cc99ab5289fe5f8f9e36458a2f632d45b7f53592
Jakub Bryska <troppout@gmail.com>
2025-03-12 09:34:36 +0100

    chore: update app.component.html with navbar and footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarVerticalComponent } from './components/navbar-vertical/navbar-vertical.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    NavbarVerticalComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ITHelper';
