4a29467e800c715d1cfecf63d0e4b91537687bd4
Jakub Bryska <troppout@gmail.com>
2025-03-12 09:33:23 +0100

    feat: add navbar and navbar-vertical components

---
+++ src/app/components/navbar-vertical/navbar-vertical.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-vertical',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-vertical.component.html',
+  styleUrl: './navbar-vertical.component.css'
+})
+export class NavbarVerticalComponent {
+
+}
