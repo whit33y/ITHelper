1ae7717c827a8acaeadcac978a01484f3d0b386e
Jakub Bryska <troppout@gmail.com>
2025-03-13 14:06:40 +0100

    chore: update navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   @Input() userName = 'Jakub Test';
   isMenuOpen = false;
+  active = 'new';
 
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = event.urlAfterRedirects.split('/').pop();
+        this.active = currentRoute || '';
+      }
+    });
+  }
 
   navigateTo(route: string) {
     this.router.navigate([route]);
