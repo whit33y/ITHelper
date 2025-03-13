384399279a6ed465a4285ffb66c88d22dc14778f
Jakub Bryska <troppout@gmail.com>
2025-03-13 10:04:13 +0100

    chore: update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,27 @@
 import { Routes } from '@angular/router';
 import { StartPageComponent } from './pages/start-page/start-page.component';
+import { NewReportComponent } from './pages/new-report/new-report.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: ReportsComponent,
+    title: 'Twoje zgłoszenia',
+  },
+  {
+    path: 'new',
+    component: NewReportComponent,
+    title: 'Stwórz zgłoszenie',
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    title: 'Ustawienia',
+  },
+  {
+    path: 'test',
     component: StartPageComponent,
     title: 'ITHelper',
   },
