4a51c307b57b10e1a0056110fea61da25e564e51
Jakub Bryska <troppout@gmail.com>
2025-03-13 15:19:45 +0100

    chore: update login logic

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { LoginFormComponent } from '../../components/elements/login-form/login-form.component';
+import { RegisterFormComponent } from '../../components/elements/register-form/register-form.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LoginFormComponent, RegisterFormComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  formType = 'login';
 }
