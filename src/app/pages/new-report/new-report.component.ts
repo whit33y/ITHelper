602296da23b90d4880660d3ba37540d41167b891
Jakub Bryska <troppout@gmail.com>
2025-03-13 10:03:51 +0100

    feat: add new report component

---
+++ src/app/pages/new-report/new-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-report.component.html',
+  styleUrl: './new-report.component.css'
+})
+export class NewReportComponent {
+
+}
