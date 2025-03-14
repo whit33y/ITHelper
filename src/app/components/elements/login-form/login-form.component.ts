730e3c4298f4301bfaa0979379eada2a33456d7a
Jakub Bryska <troppout@gmail.com>
2025-03-14 09:24:13 +0100

    chore: update login-form to output data

--- src/app/components/elements/login-form/login-form.component.ts
+++ src/app/components/elements/login-form/login-form.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
   imports: [],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+  styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
-
+  @Output() formEvent = new EventEmitter<string>();
+  changeView() {
+    this.formEvent.emit('register');
+  }
 }
