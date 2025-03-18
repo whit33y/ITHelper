d567e4fd3b6e4d3392c2159c5d8b3d4bf198d9d5
Jakub Bryska <troppout@gmail.com>
2025-03-18 10:05:09 +0100

    feat: add new report form validators

--- src/app/components/elements/new-report-form/new-report-form.component.ts
+++ src/app/components/elements/new-report-form/new-report-form.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-new-report-form',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ReactiveFormsModule],
   templateUrl: './new-report-form.component.html',
   styleUrl: './new-report-form.component.css',
 })
-export class NewReportFormComponent {}
+export class NewReportFormComponent {
+  newReport = new FormGroup({
+    title: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+    ]),
+    category: new FormControl('hardware'),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(600),
+    ]),
+    priority: new FormControl('low'),
+  });
+}
