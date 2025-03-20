1ba78e9e3ab1c87ecd2dc7871fc481f2a8811cf8
Jakub Bryska <troppout@gmail.com>
2025-03-20 09:47:03 +0100

    feat: add report card styles

--- src/app/components/elements/report-card/report-card.component.ts
+++ src/app/components/elements/report-card/report-card.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-report-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './report-card.component.html',
   styleUrl: './report-card.component.css',
 })
@@ -15,4 +16,31 @@ export class ReportCardComponent {
   @Input() category = '';
   @Input() priority = '';
   @Input() assigned_to = '';
+  @Input() created = '';
+  @Input() index?: number;
+
+  changeStatus(status: string) {
+    if (status === 'new') {
+      return 'Nowy';
+    } else if (status === 'in_progress') {
+      return 'W trakcie';
+    } else if (status === 'resolved') {
+      return 'Rozwiązany';
+    } else if (status === 'closed') {
+      return 'Zamknięte';
+    }
+    return '';
+  }
+  changePriority(priority: string) {
+    if (priority === 'low') {
+      return 'Niski';
+    } else if (priority === 'medium') {
+      return 'Średni';
+    } else if (priority === 'high') {
+      return 'Wysoki';
+    } else if (priority === 'critical') {
+      return 'Krytyczny';
+    }
+    return '';
+  }
 }
