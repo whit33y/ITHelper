34bc9c14d97177b1049ca5261e4e5be99233dd03
whit33y <troppout@gmail.com>
2025-03-25 19:36:36 +0100

    feat: add manage raport page

---
+++ src/app/pages/manage-raport/manage-raport.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-raport',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-raport.component.html',
+  styleUrl: './manage-raport.component.css'
+})
+export class ManageRaportComponent {
+
+}
