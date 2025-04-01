e361e95b115227f43fecbef9fda853b54c7abfa6
Jakub Bryska <troppout@gmail.com>
2025-04-01 14:36:27 +0200

    feat: add storage functions to upload etc.

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
+import { Databases, Storage } from 'appwrite';
+import { environment } from '../../../environment';
+import { client } from '../lib/appwrite';
+import { from, Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageService {
+  private database: Databases;
+  private storage: Storage;
+  private databaseId = environment.databaseId;
+  private storageId = environment.storageId;
+  private storageCollectionId = environment.storageCollectionId;
 
-  constructor() { }
+  constructor() {
+    this.storage = new Storage(client);
+    this.database = new Databases(client);
+  }
+
+  uploadImage(file: File): Observable<string | null> {
+    const promise = this.storage.createFile(this.storageId, 'unique()', file);
+    return from(promise).pipe(
+      map((response) => response.$id),
+      catchError((error) => {
+        console.error('Upload failed:', error);
+        return of(null);
+      })
+    );
+  }
+
+  getImagePreview(fileId: string): string {
+    return this.storage.getFilePreview(this.storageId, fileId);
+  }
+
+  deleteImage(fileId: string): Observable<boolean> {
+    const promise = this.storage.deleteFile(this.storageId, fileId);
+    return from(promise).pipe(
+      map(() => true),
+      catchError((error) => {
+        console.error('Delete failed:', error);
+        return of(false);
+      })
+    );
+  }
+
+  postFileReportId(fileId: string, reportId: string): Observable<any> {
+    return from(
+      this.database.createDocument(
+        this.databaseId,
+        this.storageCollectionId,
+        'unique()',
+        {
+          reportId,
+          fileId,
+        }
+      )
+    ).pipe(
+      map((response) => response as any),
+      catchError((error) => {
+        console.error(error);
+        return of(null);
+      })
+    );
+  }
 }
