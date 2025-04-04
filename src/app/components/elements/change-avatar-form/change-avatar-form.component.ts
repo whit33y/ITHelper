743ce8e7a0183c4de856db78963d7a0dc3cae2d6
Jakub Bryska <troppout@gmail.com>
2025-04-04 12:22:31 +0200

    chore: update on change avatar form

--- src/app/components/elements/change-avatar-form/change-avatar-form.component.ts
+++ src/app/components/elements/change-avatar-form/change-avatar-form.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { StorageService } from '../../../services/storage.service';
 
 @Component({
   selector: 'app-change-avatar-form',
   standalone: true,
   imports: [],
   templateUrl: './change-avatar-form.component.html',
-  styleUrl: './change-avatar-form.component.css'
+  styleUrl: './change-avatar-form.component.css',
 })
 export class ChangeAvatarFormComponent {
+  constructor(private storageService: StorageService) {}
 
+  @Input() userId?: string;
+
+  selectedFile?: File;
+  previewUrl?: string;
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      this.uploadImage();
+    }
+  }
+
+  addFileUserId(userId: string, fileId: string) {
+    this.storageService.postUserId(userId, fileId).subscribe({
+      next: (response) => {},
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {},
+    });
+  }
+
+  uploadImage() {
+    if (this.selectedFile) {
+      this.storageService.uploadImage(this.selectedFile).subscribe({
+        next: (fileId) => {
+          if (fileId) {
+            this.previewUrl = this.storageService.getImageLink(fileId);
+            this.addFileUserId(this.userId!, fileId);
+          }
+        },
+        error: (error) => {
+          console.error('Error uploading image:', error);
+        },
+        complete: () => {},
+      });
+    }
+  }
 }
