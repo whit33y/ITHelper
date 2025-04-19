92c4786afc53d86c5940bddf6413b9bc02c477c4
whit33y <troppout@gmail.com>
2025-04-19 17:20:47 +0200

    ./

--- src/environments/environment.template.ts
+++ src/environments/environment.template.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   projectID: '__PROJECT_ID__',
   databaseId: '__DATABASE_ID__',
   commentsCollectionId: '__COMMENTS_COLLECTION_ID__',
