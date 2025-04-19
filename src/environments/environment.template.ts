12618683df58fbc0d223abab2722e4a18c10818a
whit33y <troppout@gmail.com>
2025-04-19 17:49:11 +0200

    ./

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  projectID: '__PROJECT_ID__',
+  databaseId: '__DATABASE_ID__',
+  commentsCollectionId: '__COMMENTS_COLLECTION_ID__',
+  reportsCollectionId: '__REPORTS_COLLECTION_ID__',
+  userGroupId: '__USER_GROUP_ID__',
+  adminsGroupId: '__ADMINS_GROUP_ID__',
+  storageId: '__STORAGE_ID__',
+  storageCollectionId: '__STORAGE_COLLECTION_ID__',
+  avatarCollectionId: '__AVATAR_COLLECTION_ID__',
+};
